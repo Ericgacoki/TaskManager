f9b82abe126c437ed43aa99fd9a2bc97069419d3
Ericgacoki <gacokieric@gmail.com>
2025-10-16 00:43:41 +0300

    update existing files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.material3)
 
     // Room
